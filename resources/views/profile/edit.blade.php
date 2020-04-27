f48b95e401fc776a22a9aa274a9e72982f85a025
Mario Perez <mario.perez@tigears.com>
2020-04-26 23:14:04 -0400

    adaptada vista listado usuarios

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['activePage' => 'profile', 'titlePage' => __('User Profile')])
+@extends('layouts.app', ['activePage' => 'profile', 'titlePage' => __('Perfil usuario')])
 
 @section('content')
   <div class="content">
