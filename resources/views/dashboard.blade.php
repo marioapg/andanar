fda10da65fb9909fd3f6db3832da651fc03c7065
Mario Pérez <mperez.392@gmail.com>
2021-01-15 15:41:29 -0400

    numeros reales en el dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <i class="material-icons">content_copy</i>
               </div>
               <p class="card-category">Presupustos</p>
-              <h3 class="card-title">9
+              <h3 class="card-title">{{ \App\Budget::all()->count() }}
                 {{--<small>GB</small>--}}
               </h3>
             </div>
@@ -31,7 +31,7 @@
                   <i class="material-icons">face</i>
                 </div>
                 <p class="card-category">Clientes</p>
-                <h3 class="card-title">20</h3>
+                <h3 class="card-title">{{ \App\Client::all()->count() }}</h3>
               </div>
               <div class="card-footer">
                 <div class="stats">
@@ -69,7 +69,7 @@
                   <i class="material-icons">payment</i>
                 </div>
                 <p class="card-category">Varilleros</p>
-                <h3 class="card-title">3
+                <h3 class="card-title">{{ \App\User::where('type','technical')->count() }}
                 </h3>
               </div>
               <div class="card-footer">
