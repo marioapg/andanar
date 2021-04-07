c73b58fc90e022eb8a2dda1ac185be40e602d0bf
Mario Pérez <mperez.392@gmail.com>
2021-04-07 10:13:46 -0400

    agregado editar clientes creados por usuario normal

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -44,7 +44,7 @@
                           <i class="material-icons">face</i>
                       </span>
                     </div>
-                    <input type="text" name="name" class="form-control" placeholder="{{ __('Nombre...') }}" value="{{ $client->name }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                    <input type="text" name="name" class="form-control" placeholder="{{ __('Nombre...') }}" value="{{ $client->name }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
                   </div>
                   @if ($errors->has('name'))
                     <div id="name-error" class="error text-danger pl-3" for="name" style="display: block;">
@@ -74,7 +74,7 @@
                         <i class="material-icons">fingerprint</i>
                       </span>
                     </div>
-                    <input type="text" name="document" id="document" class="form-control" placeholder="{{ __('CIF/NIF/DNI...') }}" value="{{ $client->document }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                    <input type="text" name="document" id="document" class="form-control" placeholder="{{ __('CIF/NIF/DNI...') }}" value="{{ $client->document }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
                   </div>
                   @if ($errors->has('document'))
                     <div id="document-error" class="error text-danger pl-3" for="document" style="display: block;">
@@ -89,7 +89,7 @@
                         <i class="material-icons">house</i>
                       </span>
                     </div>
-                    <input type="text" name="address" id="address" class="form-control" placeholder="{{ __('Dirección...') }}" value="{{ $client->address }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                    <input type="text" name="address" id="address" class="form-control" placeholder="{{ __('Dirección...') }}" value="{{ $client->address }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
                   </div>
                   @if ($errors->has('address'))
                     <div id="address-error" class="error text-danger pl-3" for="address" style="display: block;">
@@ -104,7 +104,7 @@
                         <i class="material-icons">post_add</i>
                       </span>
                     </div>
-                    <input type="text" name="postal_code" id="postal_code" class="form-control" placeholder="{{ __('Código postal...') }}" value="{{ $client->postal_code }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                    <input type="text" name="postal_code" id="postal_code" class="form-control" placeholder="{{ __('Código postal...') }}" value="{{ $client->postal_code }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
                   </div>
                   @if ($errors->has('postal_code'))
                     <div id="postal_code-error" class="error text-danger pl-3" for="postal_code" style="display: block;">
@@ -119,7 +119,7 @@
                         <i class="material-icons">room</i>
                       </span>
                     </div>
-                    <input type="text" name="city" id="city" class="form-control" placeholder="{{ __('Ciudad...') }}" value="{{ $client->city }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                    <input type="text" name="city" id="city" class="form-control" placeholder="{{ __('Ciudad...') }}" value="{{ $client->city }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
                   </div>
                   @if ($errors->has('city'))
                     <div id="city-error" class="error text-danger pl-3" for="city" style="display: block;">
@@ -134,7 +134,7 @@
                         <i class="material-icons">map</i>
                       </span>
                     </div>
-                    <input type="text" name="state" id="state" class="form-control" placeholder="{{ __('Estado...') }}" value="{{ $client->state }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                    <input type="text" name="state" id="state" class="form-control" placeholder="{{ __('Estado...') }}" value="{{ $client->state }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
                   </div>
                   @if ($errors->has('state'))
                     <div id="state-error" class="error text-danger pl-3" for="state" style="display: block;">
@@ -149,7 +149,7 @@
                     <i class="material-icons">supervised_user_circle</i>
                   </span>
                 </div>
-                <input type="text" name="responsable" id="responsable" class="form-control" value="{{ $client->responsable ?? '' }}" placeholder="{{ __('Responsable...') }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                <input type="text" name="responsable" id="responsable" class="form-control" value="{{ $client->responsable ?? '' }}" placeholder="{{ __('Responsable...') }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
               </div>
               @if ($errors->has('responsable'))
                 <div id="responsable-error" class="error text-danger pl-3" for="responsable" style="display: block;">
@@ -164,7 +164,7 @@
                     <i class="material-icons">email</i>
                   </span>
                 </div>
-                <input type="text" name="contact_responsable" id="contact_responsable" class="form-control" value="{{ $client->contact_responsable ?? '' }}" placeholder="{{ __('Contacto Responsable...') }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                <input type="text" name="contact_responsable" id="contact_responsable" class="form-control" value="{{ $client->contact_responsable ?? '' }}" placeholder="{{ __('Contacto Responsable...') }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
               </div>
               @if ($errors->has('contact_responsable'))
                 <div id="contact_responsable-error" class="error text-danger pl-3" for="contact_responsable" style="display: block;">
@@ -179,7 +179,7 @@
                         <i class="material-icons">outlined_flag</i>
                       </span>
                     </div>
-                    <input type="text" name="country" id="country" class="form-control" placeholder="{{ __('País') }}" value="{{ $client->country }}" required @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                    <input type="text" name="country" id="country" class="form-control" placeholder="{{ __('País') }}" value="{{ $client->country }}" required @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
                   </div>
                   @if ($errors->has('country'))
                     <div id="country-error" class="error text-danger pl-3" for="country" style="display: block;">
@@ -194,7 +194,7 @@
                         <i class="material-icons">contact_phone</i>
                       </span>
                     </div>
-                    <input type="text" name="phone" id="phone" class="form-control" placeholder="{{ __('Teléfono...') }}" value="{{ $client->phone }}" @if(!auth()->user()->hasRole('admin')) readonly @endif>
+                    <input type="text" name="phone" id="phone" class="form-control" placeholder="{{ __('Teléfono...') }}" value="{{ $client->phone }}" @if(!auth()->user()->hasRole('admin') && ($client->created_by !== auth()->user()->id)) readonly @endif>
                   </div>
                   @if ($errors->has('phone'))
                     <div id="phone-error" class="error text-danger pl-3" for="phone" style="display: block;">
@@ -202,7 +202,7 @@
                     </div>
                   @endif
                 </div>
-                @if ( auth()->user()->hasRole('admin') )
+                @if ( auth()->user()->hasRole('admin') || ($client->created_by === auth()->user()->id) )
                   <div class="card-footer ml-auto mr-auto">
                     <button type="submit" class="btn btn-info">{{ __('Actualizar') }}</button>
                   </div>
